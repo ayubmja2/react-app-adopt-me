b42b90260fe6eb492be6152a190161775962c773
Ayub <ayub@localhost.localdomain>
2023-07-09 11:54:15 -0400

    Finished project.

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 
@@ -32,4 +33,11 @@ const Details = () => {
   );
 };
 
-export default Details;
+function DetailsErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <Details />
+    </ErrorBoundary>
+  );
+}
+export default DetailsErrorBoundary;
